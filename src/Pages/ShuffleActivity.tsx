390bc32d050bb05d98eaea52f4b7a1e34cb8504a
theeatch <dhruvnegiclub03@gmail.com>
2023-09-15 14:42:24 +0530

    shuffleactivity

--- src/Pages/ShuffleActivity.tsx
+++ src/Pages/ShuffleActivity.tsx
@@ -6,9 +6,9 @@ const ShuffActivity: React.FC = () => {
 
 
   return (
-    <div className="text-7xl h-screen flex items-center m-auto justify-between bg-[#586F7C] px-36">
-        <h1 className="text-7xl">Sound Shuffler Activity!</h1>
-        <div className="bg-white w-full">
+    <div className="text-7xl h-screen flex flex-col items-center justify-between bg-[#586F7C] p-36">
+        <h1 className="text-7xl font-semibold text-white">Sound Shuffler Activity!</h1>
+        <div className="bg-[#fff9f7] w-full h-2/3 rounded-3xl text-center p-10 flex flex-col">
             HI
         </div>
     </div>
